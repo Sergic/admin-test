da438287a7637f421ab1cb221756d97ac9e97de4
Zuzle <zuzleadmin@gmail.com>
2012-07-09 11:41:23 +0400

    редактирование пользователей

--- src/Instudies/AdminBundle/Controller/Management/UserController.php
+++ src/Instudies/AdminBundle/Controller/Management/UserController.php
@@ -2,9 +2,15 @@
 
 namespace Instudies\AdminBundle\Controller\Management;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use
+	Symfony\Bundle\FrameworkBundle\Controller\Controller,
+	Sensio\Bundle\FrameworkExtraBundle\Configuration\Route,
+	Sensio\Bundle\FrameworkExtraBundle\Configuration\Template
+;
+
+use
+	Instudies\AdminBundle\Form\Management\User\Find\ManagementUserFindType
+;
 
 /**
  * @Route("/admin/management/users")
@@ -18,7 +24,23 @@ class UserController extends Controller
     public function indexAction()
     {
 
-    	return array();
+        $findForm   = $this->createForm(new ManagementUserFindType());
+
+    	return array(
+    			'findForm' => $findForm->createView()
+    		);
 
     }
+
+    /**
+     * @Route("/edit", name="admin_management_user_edit")
+     * @Template()
+     */
+    public function editAction($id = null, $email = null)
+    {
+
+        return array();
+
+    }
+
 }
